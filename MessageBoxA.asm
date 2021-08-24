052eb2d2f9569fd0d76f2dbadc81056926758f5f
ydev <68254745+yd3v@users.noreply.github.com>
2021-08-23 22:46:07 -0300

    Update

--- MessageBoxA.asm
+++ MessageBoxA.asm
@@ -2,20 +2,25 @@ extern MessageBoxA
 extern ExitProcess
 
 section .rodata
-    Caption:    db "Hello Winapi", 0x0
-    Text:       db "H3333333333333y!", 0x0
+    Caption:    db "Hello Winapi",      0x0
+    Text:       db "H3333333333333y!",  0x0
+    Caption2:   db "Message",           0x0
+    Text2:      db "Canceled",          0x0
     global _start
 
 section .text
     _start:
-        push 2              ; Buttons and icons
-        push Caption        ; Guess
-        push Text           ; Guess too
-        push 0              ; Window Handle
-        call MessageBoxA    ; MessageBoxA(0, Text, Caption, 2);
-
-        push eax
-        cmp eax,   4        ;
-        je _start
-    _exit:
-        call ExitProcess
+        push    2              ; Buttons and icons
+        push    Caption        ; Guess
+        push    Text           ; Guess too
+        push    0              ; Window Handle
+        call    MessageBoxA    ; MessageBoxA(0, Text, Caption, 2);
+                               ;
+        push    eax            ; Return code to ExitProcess (MessageBoxA return)
+        cmp     eax,    4      ;                                    
+        je      _start         ;                                    |
+        cmp     eax,    3      ;                                    |
+        je      _cancel        ;                                    |
+                               ;                                    |
+    _exit:                     ;                                    V
+        call    ExitProcess    ;                                exit(eax)
