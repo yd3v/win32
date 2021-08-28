a29bb1d918faa7184cfa0cd77978131ddf23c862
ydev <68254745+yd3v@users.noreply.github.com>
2021-08-28 10:25:24 -0300

    Update GetFileInformationByHandle.c

--- GetFileInformationByHandle.c
+++ GetFileInformationByHandle.c
@@ -6,11 +6,12 @@
 int main() {
 	BY_HANDLE_FILE_INFORMATION Info;
 	SYSTEMTIME Time;
+	SYSTEMTIME Now;
 	
 	HANDLE Handle = CreateFile(
 		FILENAME,
-		GENERIC_READ | GENERIC_WRITE,
-		FILE_SHARE_READ | FILE_SHARE_WRITE,
+		GENERIC_READ ,
+		FILE_SHARE_READ,
 		NULL,
 		OPEN_ALWAYS, 
 		FILE_ATTRIBUTE_NORMAL,
@@ -22,18 +23,20 @@ int main() {
 		Handle,
 		&Info
 	);
+	
+	GetSystemTime(&Now);
 
 	if(!FileTimeToSystemTime(&Info.ftCreationTime, &Time)) {
 		PERROR(GetLastError());
 		return 1;
 	}
 	
-	printf(":: "FILENAME"\n\n");
-	printf("Filesize           -> %d bytes\n", Info.nFileSizeLow);
-	printf("Created at         -> %.2d/%.2d %.2d:%.2d:%.2d\n", Time.wDay, Time.wMonth, Time.wHour, Time.wMinute, Time.wSecond);
+	printf(":: "FILENAME"     (%.2d/%.2d - %.2d:%.2d:%.2d)\n\n", Now.wDay, Now.wMonth, Now.wHour, Now.wMinute, Now.wSecond);
+	printf(":: Filesize           -> %d bytes\n", Info.nFileSizeLow);
+	printf(":: Created at         -> %.2d/%.2d %.2d:%.2d:%.2d\n", Time.wDay, Time.wMonth, Time.wHour, Time.wMinute, Time.wSecond);
 	FileTimeToSystemTime(&Info.ftLastWriteTime, &Time);
-	printf("Last write time    -> %.2d/%.2d %.2d:%.2d:%.2d\n", Time.wDay, Time.wMonth, Time.wHour, Time.wMinute, Time.wSecond);
+	printf(":: Last write time    -> %.2d/%.2d %.2d:%.2d:%.2d\n", Time.wDay, Time.wMonth, Time.wHour, Time.wMinute, Time.wSecond);
 	FileTimeToSystemTime(&Info.ftLastAccessTime, &Time);
-	printf("Last access time   -> %.2d/%.2d %.2d:%.2d:%.2d\n", Time.wDay, Time.wMonth, Time.wHour, Time.wMinute, Time.wSecond);
+	printf(":: Last access time   -> %.2d/%.2d %.2d:%.2d:%.2d\n", Time.wDay, Time.wMonth, Time.wHour, Time.wMinute, Time.wSecond);
 	return 0;
 }
