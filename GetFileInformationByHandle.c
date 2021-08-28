0f6cbf80be2d347a87476007536737c1ca5d7677
ydev <68254745+yd3v@users.noreply.github.com>
2021-08-28 10:17:44 -0300

    Update

--- GetFileInformationByHandle.c
+++ GetFileInformationByHandle.c
@@ -1,29 +1,39 @@
 #include <Windows.h>
 #include "Util.h"
 
-#define FILENAME "Files\\Constants.txt"
+#define FILENAME "Util.h"
 
 int main() {
-	BY_HANDLE_FILE_INFORMATION* Info;
-	SYSTEMTIME* Time;
+	BY_HANDLE_FILE_INFORMATION Info;
+	SYSTEMTIME Time;
+	
 	HANDLE Handle = CreateFile(
 		FILENAME,
 		GENERIC_READ | GENERIC_WRITE,
 		FILE_SHARE_READ | FILE_SHARE_WRITE,
 		NULL,
-		OPEN_ALWAYS,
+		OPEN_ALWAYS, 
 		FILE_ATTRIBUTE_NORMAL,
 		NULL
 	);
-	
+
 	
 	GetFileInformationByHandle(
 		Handle,
-		Info
+		&Info
 	);
+
+	if(!FileTimeToSystemTime(&Info.ftCreationTime, &Time)) {
+		PERROR(GetLastError());
+		return 1;
+	}
 	
-	FileTimeToSystemTime(&Info->ftCreationTime, Time);
-	printf("Filesize -> %d bytes\nCreated at %lu:%lu:%lu\n", Info->nFileSizeLow, Time->wHour, Time->wMinute, Time->wSecond);
-	CloseHandle(Handle);
+	printf(":: "FILENAME"\n\n");
+	printf("Filesize           -> %d bytes\n", Info.nFileSizeLow);
+	printf("Created at         -> %.2d/%.2d %.2d:%.2d:%.2d\n", Time.wDay, Time.wMonth, Time.wHour, Time.wMinute, Time.wSecond);
+	FileTimeToSystemTime(&Info.ftLastWriteTime, &Time);
+	printf("Last write time    -> %.2d/%.2d %.2d:%.2d:%.2d\n", Time.wDay, Time.wMonth, Time.wHour, Time.wMinute, Time.wSecond);
+	FileTimeToSystemTime(&Info.ftLastAccessTime, &Time);
+	printf("Last access time   -> %.2d/%.2d %.2d:%.2d:%.2d\n", Time.wDay, Time.wMonth, Time.wHour, Time.wMinute, Time.wSecond);
 	return 0;
 }
