a65fddf1d34779a78b8185e9c3bad58de68a52cc
ydev <68254745+yd3v@users.noreply.github.com>
2021-08-28 22:09:42 -0300

    Update MouseAndKeyboard.c

--- MouseAndKeyboard.c
+++ MouseAndKeyboard.c
@@ -3,8 +3,22 @@
 #include <stdio.h>
 #include "Util.h"
 
-#define SLEEP 500
+#define SLEEP 0		// FUCK i3 5gen
 
 int main() {
-
+	POINT Point;
+	BYTE Keys[256];
+	while(1) {
+		Sleep(SLEEP);
+		system("cls");
+		GetCursorPos(&Point);
+		printf("Cursor position -> %d, %d\n", Point.x, Point.y);
+		GetKeyState(0);						// I don't even know why we use it before GetKeyboardState ¯\_(ツ )_/¯.
+		GetKeyboardState(Keys);				// What i know is if we don't use it, it will not work
+		int Key = 0;
+		puts("\n-- Keyboard state --\n");
+		for(Key; Key < sizeof Keys; Key++) {
+			if(Keys[Key]) printf("%d (%c) -> %d\n", Key, Key, Keys[Key]);
+		}
+	}
 }
